c0b92d1720541ab5825996d09ec04617b40f03b1
ezefm <ezefm9926@gmail.com>
2023-02-27 20:22:22 -0300

    base-project

---
+++ src/main/java/com/jvm/lunasouvenirs/LunaSouvenirsApplication.java
@@ -0,0 +1,13 @@
+package com.jvm.lunasouvenirs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunaSouvenirsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunaSouvenirsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jvm/lunasouvenirs/LunaSouvenirsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvm.lunasouvenirs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunaSouvenirsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
